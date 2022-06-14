a0754c1fc27372ba7913394bf04d37214dc5fc10
SurazGaire <luxheartsuraz@gmail.com>
2022-06-14 13:11:57 +0545

    feat : Changed the state of the databse each time

--- cypress/e2e/note_app.spec.cy.js
+++ cypress/e2e/note_app.spec.cy.js
@@ -1,5 +1,12 @@
 describe("Note app", function () {
   beforeEach(function () {
+    cy.request("POST", "http://localhost:3001/api/testing/reset");
+    const user = {
+      name: "Matti Luukkainen",
+      username: "mluukkai",
+      password: "salainen",
+    };
+    cy.request("POST", "http://localhost:3001/api/users/", user);
     cy.visit("http://localhost:3000");
   });
   it("front page can be opened", function () {
@@ -13,6 +20,36 @@ describe("Note app", function () {
     cy.contains("Login").click();
     cy.get("#username").type("mluukkai");
     cy.get("#password").type("salainen");
+    cy.get("#login-button").click();
     cy.contains("Matti Luukkainen logged in");
+  });
+  describe("when logged in", function () {
+    beforeEach(function () {
+      cy.contains("Login").click();
+      cy.get("input:first").type("mluukkai");
+      cy.get("input:last").type("salainen");
+      cy.get("#login-button").click();
+    });
+
+    it("a new note can be created", function () {
+      cy.contains("Create Note").click();
+      cy.get("input").type("a note created by cypress");
+      cy.contains("save").click();
+      cy.contains("a note created by cypress");
+    });
+
+    describe("and a note exists", function () {
+      beforeEach(function () {
+        cy.contains("Create Note").click();
+        cy.get("input").type("another note cypress");
+        cy.contains("save").click();
+      });
+
+      it("it can be made important", function () {
+        cy.contains("another note cypress").contains("make important").click();
+
+        cy.contains("another note cypress").contains("make not important");
+      });
+    });
   });
 });
